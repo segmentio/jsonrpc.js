6508484d96b31e77dcd114d12d1e2311f4fb69f8
Stephen Mathieson <me@stephenmathieson.com>
2016-02-09 14:03:02 -0800

    add tests and coverage

---
+++ test/index.js
@@ -0,0 +1,80 @@
+
+'use strict';
+
+const mitm = require('mitm');
+const assert = require('assert');
+const Client = require('..');
+
+describe('Client', function() {
+  let mock = null;
+
+  beforeEach(function() {
+    mock = mitm();
+  });
+
+  afterEach(function() {
+    mock.disable();
+  });
+
+  describe('.call(method, ...)', function() {
+    it('should call the correct method', function*() {
+      let called = false;
+
+      mock.on('connection', function(socket) {
+        socket.on('data', function(buf) {
+          const json = JSON.parse(buf);
+
+          assert.equal(json.method, 'Foo.Bar');
+          called = true;
+
+          socket.write(JSON.stringify({
+            id: json.id,
+            jsonrpc: '2.0',
+            result: 42
+          }));
+
+          socket.destroy();
+        });
+      });
+
+      const client = new Client('tcp://segment.dev:4003/rpc');
+      yield client.call('Foo.Bar', { foo: 'bar' });
+      client.sock.end();
+
+      assert(called, 'called the method');
+    });
+
+    describe('if the method errors', function() {
+      it('should throw the error', function*() {
+        mock.on('connection', function(s) {
+          s.on('data', function(buf) {
+            const json = JSON.parse(buf);
+
+            assert.equal(json.method, 'Foo.Bar');
+
+            s.write(JSON.stringify({
+              id: json.id,
+              jsonrpc: '2.0',
+              error: 'boom!'
+            }));
+
+            s.destroy();
+          });
+        });
+
+        let err = null;
+        const client = new Client('tcp://segment.dev:4003/rpc');
+        try {
+          yield client.call('Foo.Bar', { foo: 'bar' });
+        } catch (e) {
+          err = e;
+        }
+
+        client.sock.end();
+
+        assert(err);
+        assert.equal(err.message, 'boom!');
+      });
+    });
+  });
+});
